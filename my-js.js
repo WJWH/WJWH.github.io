a64f5b52bdfc853d1b53dd8cbfc8da3ad2ba67ef
Wander Hillen <wander@wetransfer.com>
2017-09-12 21:22:56 +0200

    Some codings

---
+++ my-js.js
@@ -0,0 +1,10 @@
+function doalert() {
+  alert('WELL TOUGH LUCK BUDDY!');
+  return 0;
+}
+
+function change_bg() {
+  var elem = document.getElementById("da_jumbotron");
+  elem.setAttribute("style","background-color: #8080ff;");
+  return 0;
+}
